a55514fc2cd41b892666b35c394521a9d151ad45
Matthew Rungwe <matthewrungwe@gmail.com>
2020-11-23 13:37:32 +0200

    Update paragraph & all details to footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,9 @@ import './footer.less';
 function Footer() {
     return (
         <div className="footer">
-            <p>Footer</p>
+            <p className="para">
+                &copy; 2020. All rights reserved. Designed | <a href="#" className="designer-link">Matthew Rungwe</a> 
+            </p>
         </div>
     );
 }
