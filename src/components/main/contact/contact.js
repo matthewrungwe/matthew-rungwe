06dff90882f8e8688b2253d2424e97d1ca643e5b
Matthew Rungwe <matthewrungwe@gmail.com>
2020-11-24 16:25:02 +0200

    Update contact component.

--- src/components/main/contact/contact.js
+++ src/components/main/contact/contact.js
@@ -3,7 +3,38 @@ import './contact.less';
 
 function Contact() {
     return (
-        <p>Contact Section</p>
+        <div className="contact-wrap">
+            <div className="heading">
+                <h2>Get in touch</h2>
+            </div>
+            <form id="contact-form">
+                <div className="input-block-pair">
+                    <div className="input-block">
+                        <label>
+                            <input type="text" name="name" placeholder="Name"/>
+                        </label>
+                    </div>
+
+                    <div className="input-block">
+                        <label>
+                            <input type="email" name="email" placeholder="Email" required />
+                        </label>
+                    </div>
+                </div>
+
+                <div className="textarea-block">
+                    <textarea placeholder="Message">
+
+                    </textarea>
+                </div>
+
+                <div className="submit-block">
+                    <label>
+                        <input type="submit" value="Send Message" />
+                    </label>
+                </div>
+            </form>
+        </div>
     );
 }
 
