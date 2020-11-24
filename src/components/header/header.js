2c147e63cf34d66cf92d1cb0053c432186e7c84a
Matthew Rungwe <matthewrungwe@gmail.com>
2020-11-24 13:04:59 +0200

    Update header component.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,53 @@
+// Components
+import Footer from '../footer/footer';
+
 // Styles
 import './header.less';
 
+// Images
+import profilePic from '../../assests/images/profile/matthew.JPG';
+
+const profile = {
+    name: 'Matthew Rungwe',
+    pic: {
+        src: profilePic
+    }
+};
+
 function Header() {
     return (
-        <h1>Hello World</h1>
+        <section className="header-wrap">
+            <article className="short-profile">
+                <div className="profile-pic">
+                    <img className="pic" src={profile.pic.src} alt="Matthew's profile" />
+                </div>            
+                <div className="download-profile-bio">
+                    <div className="download">
+
+                    </div>
+                    <div className="profile-bio">
+                    <p>
+                        Hi, I'm <span>{profile.name}</span>, a Front-End Developer 
+                        & Mentor based in Cape Town.
+                    </p>
+
+                    <p>
+                        I Design and Develop mordern, repsonsive and 
+                        interactive web applications.
+                    </p>
+                </div>
+                </div>
+            </article>
+
+            <article className="media-copyright">
+                <div className="media">
+
+                </div>
+                <div className="copyright">
+                    <Footer />
+                </div>
+            </article>
+        </section>
     );
 }
 
