445cab80029938c8a990486ab8f82e0248e66e17
Matthew Rungwe <matthewrungwe@gmail.com>
2020-11-25 10:28:35 +0200

    Update about component.

--- src/components/main/about/about.js
+++ src/components/main/about/about.js
@@ -4,7 +4,21 @@ import './about.less';
 function About() {
     return (
         <div className="about-wrap">
-            <p>About Component</p>
+            <div className="heading">
+                <h2>Hi, I'm <span>Matthew Rungwe</span>. It's nice to meet you.</h2>
+            </div>
+            <div className="info">
+                <p>
+                    My journey as a Front-End Developer started of as
+                    a curiosity and grew into a passion. Since then I 
+                    have managed to grow my skills a nd it allowed me 
+                    to become a freelancer, a mentor and a content writer. 
+                </p>
+                <p>
+                    I'm quietly confident, driven, always learning and growing 
+                    my skills one commit at a time.
+                </p>
+            </div>
         </div>
     );
 }
