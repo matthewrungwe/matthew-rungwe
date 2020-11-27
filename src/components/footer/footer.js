0c47a9309b3f928898ee616c7cf38d83964d55c0
Matthew Rungwe <matthewrungwe@gmail.com>
2020-11-27 10:54:38 +0200

    Add valid href value.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className="footer">
             <p className="para">
-                &copy; 2020. All rights reserved. Designed | <a href="#" className="designer-link">Matthew Rungwe</a> 
+                &copy; 2020. All rights reserved. Designed | <a href="mattjr.netlify.app" className="designer-link">Matthew Rungwe</a> 
             </p>
         </div>
     );
