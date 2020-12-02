9ab54e002898974824e907477505c77a74176575
Matthew Rungwe <matthewrungwe@gmail.com>
2020-12-02 15:28:21 +0200

    Update portfolio section.

--- src/components/main/portfolio/portfolio.js
+++ src/components/main/portfolio/portfolio.js
@@ -11,22 +11,30 @@ function Portfolio() {
             <div className="projects">
                 <div className="project one">
                     <div className="overlay">
-                        <p>Math WordHub</p>
+                        <a href="https://wordhub.netlify.app" target="_blank" rel="noreferrer" >
+                            <p>Math WordHub</p>
+                        </a>
                     </div>
                 </div>
                 <div className="project two">
                     <div className="overlay">
-                        <p>Rock Paper</p>
+                        <a href="https://matt-chorebot.netlify.app/" target="_blank" rel="noreferrer" >
+                            <p>Chore Bot Game</p>
+                        </a>
                     </div>
                 </div>
                 <div className="project three">
                     <div className="overlay">
-                        <p>Signup Page</p>
+                        <a href="https://matt-signup-page.netlify.app/" target="_blank" rel="noreferrer" >
+                            <p>Signup Page</p>
+                        </a>
                     </div>
                 </div>
                 <div className="project four">
                     <div className="overlay">
-                        <p>Todo List</p>
+                        <a href="https://matt-productcode-generator.netlify.app/" target="_blank" rel="noreferrer" >
+                            <p>Product Code Gen</p>
+                        </a>
                     </div>
                 </div>
             </div>
