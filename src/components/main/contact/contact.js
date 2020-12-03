7fe662656869ec04702535d7ee3c1511813f92da
Matthew Rungwe <matthewrungwe@gmail.com>
2020-12-03 15:59:05 +0200

    Update contact form.

--- src/components/main/contact/contact.js
+++ src/components/main/contact/contact.js
@@ -8,7 +8,7 @@ function Contact() {
                 <h2>Get in touch</h2>
                 <p>How can I help? You can leave a message and I will get back to you.</p>
             </div>
-            <form id="contact-form" name="contact-form" method="POST" data-netlify="true">
+            <form id="contact-form" name="contact-form" action="POST" netlify >
                 <div className="input-block-pair">
                     <div className="input-block">
                         <label>
@@ -24,7 +24,7 @@ function Contact() {
                 </div>
 
                 <div className="textarea-block">
-                    <textarea placeholder="Message">
+                    <textarea placeholder="Message" name="message">
 
                     </textarea>
                 </div>
